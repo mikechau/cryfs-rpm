c369167a549a0e7e3ca108bfc328d54577c183ae
Mike Chau <mike@chau.io>
2017-09-11 18:15:59 -0700

    Add travisci support

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 DIST_DIR="/build/dist"
 
-docker build --rm --tag=cryfs .
+docker build --rm --tag=cryfs . --pull
 rm -rf dist && mkdir -p dist
 docker run --rm -v `pwd`/dist:"${DIST_DIR}" -e LOCAL_USER_ID=`id -u $USER` cryfs "/build/make.sh"
